d1341c0f5ff06cb5f5f111bfa70bae037d43cc63
Shoislom1213 <152617113+Shoislom1213@users.noreply.github.com>
2026-03-26 01:07:40 +0500

    Tayyor
    Fix function definition and handle None case for URL.

--- models/audio/selection/top_windows.py
+++ models/audio/selection/top_windows.py
@@ -3,8 +3,9 @@ import json
 from difflib import SequenceMatcher
 from utils import get_video_path
 
-def get_video_duration(url = get_video_path()):
-    
+def get_video_duration(url)
+    if url is None
+        url = get_video_path())
     cmd = [
         "ffprobe", "-v", "error",
         "-show_entries", "format=duration",
