576c49215aa4f9d37a7bf837ce2214eb17ecb0a7
Shoislom1213 <152617113+Shoislom1213@users.noreply.github.com>
2026-03-26 01:10:46 +0500

    Corrected syntax in get_video_duration function
    Fix syntax error in get_video_duration function.

--- models/audio/selection/top_windows.py
+++ models/audio/selection/top_windows.py
@@ -5,7 +5,7 @@ from utils import get_video_path
 
 def get_video_duration(url)
     if url is None
-        url = get_video_path())
+        url = get_video_path()
     cmd = [
         "ffprobe", "-v", "error",
         "-show_entries", "format=duration",
